44fd72413ef0b69467f83369497e83a45703ebd4
niyaj-seikh-2003 <niyazshkk@gmail.com>
2026-09-01 23:46:39 +0530

    by niyaj

---
+++ src/String/Reverse_String.java
@@ -0,0 +1,17 @@
+package String;
+
+public class Reverse_String {
+
+	public static void main(String[] args) {
+		 String str = "Java is easy";
+	        String rev = "";
+
+	        for (int i = str.length() - 1; i >= 0; i--) {
+	            rev = rev + str.charAt(i);
+	        }
+
+	        System.out.println("Original String: " + str);
+	        System.out.println("Reverse String: " + rev);
+	}
+
+}
