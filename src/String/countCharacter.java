9d3a7219971c94db1d94e643dd9b889da81c0450
niyaj-seikh-2003 <niyazshkk@gmail.com>
2026-09-01 23:49:54 +0530

    niyaj

---
+++ src/String/countCharacter.java
@@ -0,0 +1,14 @@
+package String;
+
+public class countCharacter {
+
+	public static void main(String[] args) {
+		 String str = "Java is easy";
+
+	        int count = str.length();
+
+	        System.out.println("Total number of characters: " + count);
+
+	}
+
+}
